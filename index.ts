9932aa19c6716024b5d34e61f93419791abf55a8
Filip Chalupa <chalupa.filip@gmail.com>
2024-10-18 14:12:49 +0200

    Add base for web sockets

--- index.ts
+++ index.ts
@@ -1,15 +1,41 @@
 import { serve } from '@hono/node-server'
+import { createNodeWebSocket } from '@hono/node-ws'
 import { readFileSync } from 'fs'
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static'
+import type { WSContext } from 'hono/ws'
 
 const app = new Hono()
+
+const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app })
+
+const webSocketClients = new Set<WSContext>()
+
 app.get('/cgi-bin/epos/service.cgi', (context) => {
 	context.header('Content-Type', 'text/xml')
 	return context.body(
 		'<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><parameter xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print"><devid>local_printer</devid><printjobid></printjobid></parameter></s:Header><s:Body><response success="true" code="" status="251658262" battery="0" xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print"></response></s:Body></s:Envelope>',
 	)
 })
+app.get(
+	'/stream',
+	upgradeWebSocket(() => {
+		return {
+			onOpen: (event, context) => {
+				console.log('Connection opened')
+				webSocketClients.add(context)
+			},
+			onMessage(event, context) {
+				console.log(`Message from client: ${event.data}`)
+				context.send('Hello from server!')
+			},
+			onClose: (event, context) => {
+				console.log('Connection closed')
+				webSocketClients.delete(context)
+			},
+		}
+	}),
+)
 app.use(
 	'/*',
 	serveStatic({
@@ -20,7 +46,7 @@ app.use(
 	}),
 )
 
-serve(
+const server = serve(
 	{
 		fetch: app.fetch,
 		port:
@@ -28,6 +54,7 @@ serve(
 			100 /* The intended port is 80 but Linux gives permission denied on such a privileged port */,
 	},
 	(info) => {
-		console.log(`Listening EPOS on http://localhost:${info.port}`)
+		console.log(`Listening to EPOS on http://localhost:${info.port}`)
 	},
 )
+injectWebSocket(server)
--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-console.log('Hello from script.js')
+const delay = (milliseconds) =>
+	new Promise((resolve) => setTimeout(resolve, milliseconds))
+
+const connect = () => {
+	console.log('Connecting to server…')
+	const webSocket = new WebSocket('stream')
+
+	webSocket.addEventListener('open', () => {
+		console.log('Connected to server.')
+		webSocket.send('Hello from client!')
+	})
+
+	webSocket.addEventListener('close', async () => {
+		console.warn('Connection closed.')
+		await delay(1000)
+		connect()
+	})
+}
+
+connect()
