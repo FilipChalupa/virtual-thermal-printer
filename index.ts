247720696f73f3a59174827e845f267327ca629d
Filip Chalupa <chalupa.filip@gmail.com>
2024-10-18 13:44:24 +0200

    Serve static files

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
 import { serve } from '@hono/node-server'
+import { readFileSync } from 'fs'
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static'
 
 const app = new Hono()
-app.get('/', (c) => c.text('Hello Node.js!'))
+app.get('/cgi-bin/epos/service.cgi', (context) => {
+	context.header('Content-Type', 'text/xml')
+	return context.body(
+		'<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><parameter xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print"><devid>local_printer</devid><printjobid></printjobid></parameter></s:Header><s:Body><response success="true" code="" status="251658262" battery="0" xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print"></response></s:Body></s:Envelope>',
+	)
+})
+app.use(
+	'/*',
+	serveStatic({
+		root: './public',
+		getContent: async (path) => {
+			return readFileSync(path, 'utf-8')
+		},
+	}),
+)
 
 serve(
 	{
