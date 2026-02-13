b4cb59df7a4f265609155811d4b4ac4506749ec2
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-13 15:06:55 +0100

    Add hostname support in command-line arguments and server configuration

--- main.ts
+++ main.ts
@@ -26,8 +26,8 @@ const appVersion = (() => {
 console.log(`App version: ${appVersion}`)
 
 const flags = parseArgs(Deno.args, {
-	string: ['http', 'socket'],
-	default: { 'http': '80', 'socket': '9100' },
+	string: ['http', 'socket', 'hostname'],
+	default: { 'http': '80', 'socket': '9100', 'hostname': '127.0.0.1' },
 })
 
 function validatePort(portValue: string | number, portName: string): number {
@@ -40,6 +40,7 @@ function validatePort(portValue: string | number, portName: string): number {
 
 const httpPort = validatePort(flags['http'], 'HTTP')
 const socketPort = validatePort(flags['socket'], 'Socket')
+const hostname = flags['hostname']
 
 const app = new Hono()
 
@@ -120,6 +121,7 @@ app.use(
 Deno.serve(
 	{
 		port: httpPort,
+		hostname,
 		onListen(localAddress) {
 			console.log(
 				`Listening to HTTP on http://${localAddress.hostname}:${localAddress.port}.`,
@@ -132,8 +134,9 @@ Deno.serve(
 if (!Deno.env.get('DENO_DEPLOYMENT_ID')) {
 	const escposListener = Deno.listen({
 		port: socketPort,
+		hostname,
 	})
-	console.log(`Listening to Socket on 0.0.0.0:${socketPort}.`)
+	console.log(`Listening to Socket on ${hostname}:${socketPort}.`)
 
 	for await (const conn of escposListener) {
 		;(async () => {
