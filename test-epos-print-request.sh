88eb0b99649201f81c61d9f79ddd44c397bc2746
Filip Chalupa <chalupa.filip@gmail.com>
2026-04-21 14:28:56 +0200

    fix: update file paths in test script to include package directory for fixtures

--- test-epos-print-request.sh
+++ test-epos-print-request.sh
@@ -1,3 +1,3 @@
-curl -X POST -H "Content-Type: text/xml" -d @fixtures/request1.xml http://127.0.0.1:8000/cgi-bin/epos/service.cgi
+curl -X POST -H "Content-Type: text/xml" -d @packages/virtual-thermal-printer/fixtures/request1.xml http://127.0.0.1:8000/cgi-bin/epos/service.cgi
 
-curl -X POST -H "Content-Type: text/xml" -d @fixtures/request2.xml http://127.0.0.1:8000/cgi-bin/epos/service.cgi
+curl -X POST -H "Content-Type: text/xml" -d @packages/virtual-thermal-printer/fixtures/request2.xml http://127.0.0.1:8000/cgi-bin/epos/service.cgi
